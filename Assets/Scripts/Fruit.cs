42573c3b284294029650c76dbf81cb6ceb60c634
Erika Ruiz <106936681+minerikart@users.noreply.github.com>
2024-11-25 08:43:43 +0100

    Manage score

--- Assets/Scripts/Fruit.cs
+++ Assets/Scripts/Fruit.cs
@@ -5,43 +5,52 @@ public class Fruit : MonoBehaviour
     public GameObject whole;
     public GameObject sliced;
 
-    private Rigidbody fruitRigidBody;
+    private Rigidbody fruitRigidbody;
     private Collider fruitCollider;
-    private ParticleSystem juiceParticleEffect;
+    private ParticleSystem juiceEffect;
+
+    public int points = 1;
 
     private void Awake()
     {
-        fruitRigidBody = GetComponent<Rigidbody>();
+        fruitRigidbody = GetComponent<Rigidbody>();
         fruitCollider = GetComponent<Collider>();
-        juiceParticleEffect = GetComponentInChildren<ParticleSystem>();
+        juiceEffect = GetComponentInChildren<ParticleSystem>();
     }
 
     private void Slice(Vector3 direction, Vector3 position, float force)
     {
-        whole.SetActive(false);
-        sliced.SetActive(true);
+        GameManager.Instance.IncreaseScore(points);
 
+        // Disable the whole fruit
         fruitCollider.enabled = false;
-        juiceParticleEffect.Play();
+        whole.SetActive(false);
 
+        // Enable the sliced fruit
+        sliced.SetActive(true);
+        juiceEffect.Play();
+
+        // Rotate based on the slice angle
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         sliced.transform.rotation = Quaternion.Euler(0f, 0f, angle);
 
         Rigidbody[] slices = sliced.GetComponentsInChildren<Rigidbody>();
 
+        // Add a force to each slice based on the blade direction
         foreach (Rigidbody slice in slices)
         {
-            slice.velocity = fruitRigidBody.velocity;
+            slice.velocity = fruitRigidbody.velocity;
             slice.AddForceAtPosition(direction * force, position, ForceMode.Impulse);
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player"))
+        if (other.CompareTag("Player"))
         {
             Blade blade = other.GetComponent<Blade>();
             Slice(blade.direction, blade.transform.position, blade.sliceForce);
         }
     }
+
 }
